46c696497004e4c592b6b8b8a6c229dfac091093
yohannesjoya <yohannesdestagebru10@gmail.com>
2023-08-07 09:13:18 +0300

    Day2 Task2 Completed

---
+++ Day2/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WordFrequencyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            var input = Console.ReadLine();
+            var frequency = WordFrequency(input);
+            Console.WriteLine("Word frequency:");
+            foreach (var item in frequency)
+            {
+                Console.WriteLine("{0}: {1}", item.Key, item.Value);
+            }
+        }
+
+        public static Dictionary<string, int> WordFrequency(string input)
+        {
+            var wordFrequency = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            var words = Regex.Split(input, @"\W+");
+            foreach (var word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    if (wordFrequency.ContainsKey(word))
+                    {
+                        wordFrequency[word]++;
+                    }
+                    else
+                    {
+                        wordFrequency[word] = 1;
+                    }
+                }
+            }
+            return wordFrequency;
+        }
+    }
+}
