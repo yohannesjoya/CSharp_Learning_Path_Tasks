8557f00edf071e4593ea659c3e129df5ffc8b17f
yohannesjoya <yohannesdestagebru10@gmail.com>
2023-08-07 09:29:01 +0300

    Day2 Task3 Completed

---
+++ Day2/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Program { 
+
+    class Program
+    { 
+
+        static void Main(string[] args){
+            Console.WriteLine("*****PALINDROME CHECKER");
+            Console.WriteLine("Enter your word");
+            var input = Console.ReadLine();
+            bool ans = IsPalindrome(input);
+            Console.WriteLine($"**ANSWER*** Your Word is {(ans ? "" : "not ")}palindrome");  
+            
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            var strippedInput = Regex.Replace(input, @"\W+", "").ToLower();
+            var reversedInput = new string(strippedInput.ToCharArray().Reverse().ToArray());
+            return strippedInput == reversedInput;
+        }
+
+
+    }
+
+
+}
